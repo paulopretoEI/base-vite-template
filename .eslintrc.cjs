d94d621e0c96b4210021ba17fbb309f71af44177
Paulo Preto <paulopreto.dev@gmail.com>
2023-08-26 20:21:32 +0100

    misc

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,6 +1,9 @@
 /* eslint-env node */
 module.exports = {
-	env: { browser: true, es2020: true },
+	env: {
+		browser: true,
+		es2020: true,
+	},
 	extends: [
 		"eslint:recommended",
 		"plugin:@typescript-eslint/recommended",
